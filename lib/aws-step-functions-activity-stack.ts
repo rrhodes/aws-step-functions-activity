05026d0ed4df9c30589f0302a11e082928341920
Ross Rhodes <trh@trustpilot.com>
2022-01-30 15:28:07 +0000

    Initial commit

---
+++ lib/aws-step-functions-activity-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AwsStepFunctionsActivityStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'AwsStepFunctionsActivityQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
