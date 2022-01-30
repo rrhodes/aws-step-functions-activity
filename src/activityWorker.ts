3227b6906280d4ccf19768262babbf12303ce65a
Ross Rhodes <trh@trustpilot.com>
2022-01-30 17:28:36 +0000

    add initial polling logic to lambda function

--- src/activityWorker.ts
+++ src/activityWorker.ts
@@ -1,5 +1,22 @@
-import { ScheduledHandler } from "aws-lambda";
+import { ScheduledHandler } from 'aws-lambda';
+import { StepFunctions } from 'aws-sdk';
 
-export const handler = async (event: ScheduledHandler) => {
-  console.log(event);
+const { ACTIVITY_ARN } = process.env;
+const LOG = require('simple-node-logger').createSimpleLogger();
+const STEP_FUNCTIONS_CLIENT = new StepFunctions();
+
+export const handler = async (_event: ScheduledHandler) => {
+  LOG.info('Activity worker invoked');
+  if (!ACTIVITY_ARN) {
+    throw Error('Activity ARN from environment variables is undefined')
+  }
+
+  LOG.debug('Polling for activity task');
+  const activityTask = await STEP_FUNCTIONS_CLIENT.getActivityTask({
+    activityArn: ACTIVITY_ARN,
+    workerName: 'example-activity-worker',
+  }).promise();
+
+  LOG.info(activityTask);
+  LOG.info('Activity worker finished');
 }
